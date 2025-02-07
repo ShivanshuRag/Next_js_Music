e9de084513c86f6d90c2c8c79c887bb512d393ea
ShivanshuRag <ragsingh773@gmail.com>
2025-02-07 12:11:35 +0530

    Add HeroSection to Home page and implement Spotlight and MovingBorder components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import HeroSection from "@/components/herosection";
+
 
 
 export default function Home() {
   return (
-    <h1 className="text-2xl text-center"> Hello, I am shivanshu </h1>
+   <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+    <HeroSection/>
+
+   </main>
+
+
   );
 }
