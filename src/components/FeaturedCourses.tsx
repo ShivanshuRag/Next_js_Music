4f0bfdea59b896091f5f75767e3da81dd92f1fde
ShivanshuRag <ragsingh773@gmail.com>
2025-02-10 08:50:26 +0530

    Replace "View All courses" link with a styled Button component in FeaturedCourses

--- src/components/FeaturedCourses.tsx
+++ src/components/FeaturedCourses.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 import courseData from "../data/music_coures.json"
 import { BackgroundGradient } from "./ui/background-gradient"
 import Image from 'next/image';
-
+import { Button } from "./ui/moving-border"
 interface Course{
     id: number,
     title: string,
@@ -57,9 +57,14 @@ function FeaturedCourses() {
         </div>
         <div className="mt-20 text-center">
             <Link href={"/courses"}
-            className="px-4 py-2 rounded border border-neutral-600 text-neutral-700 bg-white hover:bg-gray-100 transition duration-200"
-            >
-            View All courses
+             
+             >
+              <Button
+                borderRadius="1.75rem"
+                className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
+                >
+               view all courses
+                </Button>
             </Link>
         </div>
     </div>
