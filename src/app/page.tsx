b51ca335d72227e52b3b3c5af925f852d2d9ed45
ShivanshuRag <ragsingh773@gmail.com>
2025-02-10 11:08:31 +0530

    Add WhyChooseUs component and StickyScroll for enhanced user engagement on Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/herosection";
+import WhyChooseUs from "@/components/whyChooseUs";
 
 
 
@@ -8,7 +9,7 @@ export default function Home() {
    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
     <HeroSection/>
      <FeaturedCourses/>
-
+     <WhyChooseUs/>
    </main>
 
 
